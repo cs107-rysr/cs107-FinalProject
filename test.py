f9ff58f0396ddfde1160ad4f7e55fa95ca3af9b7
Yuanbiao (Ted) Wang <52434628+agil27@users.noreply.github.com>
2021-10-05 15:33:02 -0400

    "new unittest and readme"

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
-def main():
-    print('hello world')
+import unittest
+import spladtool as st
+
+
+class TestSquare(unittest.TestCase):
+    def test_square(self):
+        x = st.tensor([4.0])
+        y = st.square(x)
+        y.backward()
+        self.assertEqual(y, st.tensor([16.0]))
+        self.assertEqual(x.grad_, st.tensor([8.0])
+
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
