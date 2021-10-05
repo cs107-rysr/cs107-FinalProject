c4baa5a3fb79c8848076a828556b23fd69363583
Yuanbiao (Ted) Wang <52434628+agil27@users.noreply.github.com>
2021-10-05 16:01:01 -0400

    fix more bugs and reduce the python versions

--- spladtool/tensor.py
+++ spladtool/tensor.py
@@ -1,11 +1,14 @@
 import numpy as np 
 
 class Tensor():
-    def __init__(self, x):
+    def __init__(self, x=None):
         super().__init__()
-        assert type(x) in [np.array, list]
-        if type(x) == list:
-            x = np.array(x)
+        if x = None:
+            self.data = None
+        else:
+            assert type(x) in [np.array, list]
+            if type(x) == list:
+                x = np.array(x)
         self.data = x
         self.grad = None
         self.dependency = None
