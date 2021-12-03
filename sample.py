a9ca2e51e88afbacb72d8878c4cfb3de20690d4d
ryejulson <ryegun242@gmail.com>
2021-12-03 11:59:35 -0500

    Decorated sample.py a little bit for easier manual testing

--- sample.py
+++ sample.py
@@ -1,10 +1,14 @@
 import unittest
 import numpy as np 
-import spladtool_reverse as str
+import spladtool_reverse_rye as str
 
-
-x = str.tensor([1., 2.])
+print('------------------------In reverse mode------------------------')
+print('------------------------Two inputs-----------------------------')
+print('Inputs:')
+x = str.Tensor([1., 2.])
 y = 2 * x + 1
+print('x : ', x)
+print('y : ', y)
 # x
 # a = x
 # b = x
@@ -16,9 +20,25 @@ y = 2 * x + 1
 # z = g * f
 # z.backward()
 z = - y / (x ** 3)
+z.backward() # execute backward lazily
+print('Results:')
+print('z: ', z)
+print('x.grad', x.grad) #dz/dx
+print('y.grad', y.grad) #dz/dy
+
 print('------------------------In reverse mode------------------------')
+print('------------------------One input-----------------------------')
+x = str.Tensor([2])
+y = str.Tensor([2])
+print('Input:')
 print('x : ', x)
-print('y : ', y)
+# w1 = x
+# w2 = w1^3
+# z = w2
+# z.backward()
+print('Results:')
+z = str.exp(x*y)
 print('z: ', z)
 z.backward() # execute backward lazily
 print('x.grad', x.grad) #dz/dx
+print('y.grad', y.grad) #dz/dx
