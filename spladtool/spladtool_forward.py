3f7cde47ab70ccbf451f7bd81a33876541dd4d83
SHIHAN LIN <shihanlin@g.harvard.edu>
2021-12-09 08:36:14 -0500

    forward mode docstring example

--- spladtool/spladtool_forward.py
+++ spladtool/spladtool_forward.py
@@ -442,39 +442,207 @@ def log_base(x: Tensor, base: float):
     return Log_Base()(x, base)
 
 def sin(x: Tensor):
+    '''
+    Compute the sine of a Tensor object
+    
+    Parameters
+    ----------
+    x : Tensor
+    
+    Examples
+    -------
+    >>> x = tensor([[1., 2.], [3., 4.]])
+    >>> z = sin(x)
+    >>> print(z)
+    spladtool.Tensor([[ 0.84147098, 0.90929743], [ 0.14112001, -0.7568025 ]])
+    '''
     return Sin()(x)
 
 def cos(x: Tensor):
+    '''
+    Compute the cosine of a Tensor object
+    
+    Parameters
+    ----------
+    x : Tensor
+    
+    Examples
+    -------
+    >>> x = tensor([[1., 2.], [3., 4.]])
+    >>> z = cos(x)
+    >>> print(z)
+    spladtool.Tensor([[0.54030231, -0.41614684], [-0.9899925, -0.65364362]])
+    '''
     return Cos()(x)
 
 def tan(x: Tensor):
+    '''
+    Compute the tangent of a Tensor object
+    
+    Parameters
+    ----------
+    x : Tensor
+    
+    Examples
+    -------
+    >>> x = tensor([[1., 2.], [3., 4.]])
+    >>> z = tan(x)
+    >>> print(z)
+    spladtool.Tensor([[1.55740772, -2.18503986], [-0.14254654, 1.15782128]])
+    '''
     return Tan()(x)
 
 def arcsin(x: Tensor):
+    '''
+    Compute the arcsine of a Tensor object
+    
+    Parameters
+    ----------
+    x : Tensor
+    
+    Examples
+    -------
+    >>> x = tensor([[0.1, 0.2], [0.3, 0.4]])
+    >>> z = arcsin(x)
+    >>> print(z)
+    spladtool.Tensor([[1.47062891, 1.36943841], [1.26610367, 1.15927948]])
+    '''
     return ArcSin()(x)
 
 def arccos(x: Tensor):
+    '''
+    Compute the arccosine of a Tensor object
+    
+    Parameters
+    ----------
+    x : Tensor
+    
+    Examples
+    -------
+    >>> x = tensor([[0.1, 0.2], [0.3, 0.4]])
+    >>> z = arccos(x)
+    >>> print(z)
+    spladtool.Tensor([[0.09966865, 0.19739556], [0.29145679, 0.38050638]])
+    '''
     return ArcCos()(x)
 
 def arctan(x: Tensor):
+    '''
+    Compute the arctangent of a Tensor object
+    
+    Parameters
+    ----------
+    x : Tensor
+    
+    Examples
+    -------
+    >>> x = tensor([[0.1, 0.2], [0.3, 0.4]])
+    >>> z = arctan(x)
+    >>> print(z)
+    sinh: spladtool.Tensor([[0.10016742, 0.20135792], [0.30469265 0.41151685]])
+    '''
     return ArcTan()(x)
 
 def sinh(x: Tensor):
+    '''
+    Compute the hyperbolic sine of a Tensor object
+    
+    Parameters
+    ----------
+    x : Tensor
+    
+    Examples
+    -------
+    >>> x = tensor([[1., 2.], [3., 4.]])
+    >>> z = sinh(x)
+    >>> print(z)
+    spladtool.Tensor([[1.17520119, 3.62686041], [10.01787493, 27.2899172 ]])
+    '''
     return Sinh()(x)
 
 def cosh(x: Tensor):
+    '''
+    Compute the hyperbolic cosine of a Tensor object
+    
+    Parameters
+    ----------
+    x : Tensor
+    
+    Examples
+    -------
+    >>> x = tensor([[1., 2.], [3., 4.]])
+    >>> z = cosh(x)
+    >>> print(z)
+    spladtool.Tensor([[ 1.54308063, 3.76219569], [10.067662, 27.30823284]])
+    '''
     return Cosh()(x)
 
 def tanh(x: Tensor):
+    '''
+    Compute the hyperbolic tangent of a Tensor object
+    
+    Parameters
+    ----------
+    x : Tensor
+    
+    Examples
+    -------
+    >>> x = tensor([[1., 2.], [3., 4.]])
+    >>> z = tanh(x)
+    >>> print(z)
+    spladtool.Tensor([[0.76159416, 0.96402758], [0.99505475, 0.9993293 ]])
+    '''
     return Tanh()(x)
 
 def logistic(x: Tensor):
+    '''
+    Apply logistic function to a Tensor object
+    
+    Parameters
+    ----------
+    x : Tensor
+    
+    Examples
+    -------
+    >>> x = tensor([[-1., 2.], [-3., 4.]])
+    >>> z = logistic(x)
+    >>> print(z)
+    spladtool.Tensor([[0.73105858, 0.88079708], [0.95257413, 0.98201379]])
+    '''
     return Logistic()(x)
 
 def sqrt(x: Tensor):
+    '''
+    Compute the square root of a Tensor object
+    
+    Parameters
+    ----------
+    x : Tensor
+    
+    Examples
+    -------
+    >>> x = tensor([[4., 4.], [16., 16.]])
+    >>> z = sqrt(x)
+    >>> print(z)
+    spladtool.Tensor([[12., 2.], [4., 4.]])
+    '''
     return SquareRoot()(x)
 
 def abs(x: Tensor):
+    '''
+    Compute the absolute value of a Tensor object
+    
+    Parameters
+    ----------
+    x : Tensor
+    
+    Examples
+    -------
+    >>> x = tensor([[-1., 2.], [-3., 4.]])
+    >>> z = abs(x)
+    >>> print(z)
+    spladtool.Tensor([[1., 2.], [3., 4.]])
+    '''
     return Abs()(x)
 
 def equal(x: Tensor, y):
@@ -1134,4 +1302,3 @@ class GreaterEqual(Comparator):
     def __init__(self):
         super().__init__(np.greater_equal)
         self.desc = 'spladtool.Layer.GreaterEqual'
-
